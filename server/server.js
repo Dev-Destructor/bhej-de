a1e8df50d14c0a72ddbda36a7eeb20af5d30a1ca
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-07-09 21:06:31 +0530

    server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config({ path: "./env/secrets.env" });
+const app = require('./app');
+const http = require('http');
+
+const server = http.createServer(app);
+const { PORT } = process.env;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
