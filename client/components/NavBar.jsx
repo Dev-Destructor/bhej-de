1f94f30905eb9c191fd8f029b10055275fa9f3a8
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-09-13 23:06:12 +0530

    added react-scroll

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 function NavBar() {
   return (
-    <nav>
+    <nav className="sticky top-0 z-50 bg-white">
       <div>
         <ul className="flex justify-center p-2">
           <li className="m-3 md:m-4 px-3 md:px-8 md:text-xl cursor-pointer text-gray-600 transition duration-100 transform hover:scale-125 hover:text-black">
-            <a href="#home">Home</a>
+            <Link
+              to={"upload"}
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-70}
+              className="transition-all duration-300"
+            >
+              Upload
+            </Link>
           </li>
           <li className="m-3 md:m-4 px-3 md:px-8 md:text-xl cursor-pointer text-gray-600 transition duration-100 transform hover:scale-125 hover:text-black">
-            <a href="#upload">Upload</a>
-          </li>
-          <li className="m-3 md:m-4 px-3 md:px-8 md:text-xl cursor-pointer text-gray-600 transition duration-100 transform hover:scale-125 hover:text-black">
-            <a href="#download">Download</a>
+          <Link
+              to={"download"}
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-70}
+              className="transition-all duration-300"
+            >
+              Download
+            </Link>
           </li>
         </ul>
       </div>
